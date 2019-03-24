846d6e900bb86fdae363c7820a708310a0f543fa
Leon-Arno <leon.rimrod@gmail.com>
2019-03-24 01:48:24 +0100

    refactor

--- HTML & CSS/Overlay-form/js/index.js
+++ HTML & CSS/Overlay-form/js/index.js
@@ -1,12 +1,10 @@
-const elements ={
-  signUpButton = document.getElementById('signUp'),
-  signInButton = document.getElementById('signUp'),
-  container = document.getElementById('container')
-}
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
 
-elements.signUpButton.addEventListener('click',() =>{
-  container.classList.add('active-panel')
-})
-elements.signInButton.addEventListener('click', () => {
-  container.classList.remove('active-panel')
-})
+signUpButton.addEventListener('click', () => {
+  container.classList.add('active-panel');
+});
+signInButton.addEventListener('click', () => {
+  container.classList.remove('active-panel');
+});
